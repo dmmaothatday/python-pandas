26078e72291465555315b3b4cbbe19dda8a9e66b
ThuyDung1911 <103367661+ThuyDung1911@users.noreply.github.com>
2023-11-24 23:12:16 +0700

    Update main

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import pandas as pd
+# a. Đọc dữ liệu từ Excel
+df_san_pham = pd.read_excel('DuLieuThucHanh2_V1.xlsx',sheet_name = 'San Pham')
+df_nhan_vien = pd.read_excel('DuLieuThucHanh2_V1.xlsx',sheet_name = 'Nhan Vien')
+df_hoa_don = pd.read_excel('DuLieuThucHanh2_V1.xlsx',sheet_name = 'Hoa Don')
+df_thong_tin = pd.read_excel('DuLieuThucHanh2_V1.xlsx',sheet_name = 'Thong Tin Hoa Don')
+# Hiển thị DataFrames
+print(df_san_pham)
+print(df_nhan_vien)
+print(df_hoa_don)
+print(df_thong_tin)
+# b. Tạo DataFrame mới với số lượng sản phẩm đã bán
+df_ban_hang = df_thong_tin.groupby('ID San Pham')['So Luong'].sum().reset_index()
+df_ban_hang = df_ban_hang.merge(df_san_pham[['ID San Pham', 'Ten']], on='ID San Pham')
+print(df_ban_hang)
+# Sản phẩm bán chạy nhất
+best_selling = df_ban_hang[df_ban_hang['So Luong'] == df_ban_hang['So Luong'].max()]
+print(best_selling)
